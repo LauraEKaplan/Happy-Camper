bc4eb4db5a2c8b183cdce820323ced86093f8a62
LauraEKaplan <LauraKaplan.E@gmail.com>
2021-11-14 17:58:24 -0500

    First Push! Fingers Crossed.

---
+++ HappyCamper/src/happyCamper/HappyCamper.java
@@ -0,0 +1,14 @@
+package happyCamper;
+
+public class HappyCamper {
+
+	public HappyCamper() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
