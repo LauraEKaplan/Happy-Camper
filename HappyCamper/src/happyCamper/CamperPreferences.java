101c294d61105453869802cf47acf6128ffec93f
vjacz <vjacz@DESKTOP-83LPRIO.fios-router.home>
2021-11-14 23:44:27 -0500

    ++

--- HappyCamper/src/happyCamper/CamperPreferences.java
+++ HappyCamper/src/happyCamper/CamperPreferences.java
@@ -1,5 +1,6 @@
 package happyCamper;
 
+<<<<<<< Upstream, based on origin/Finalized_Project
 public class CamperPreferences 
 {
 	private static boolean preferredRain;
@@ -26,9 +27,59 @@ public class CamperPreferences
  	
 	public CamperPreferences() 
 	{
+=======
+
+public class CamperPreferences 
+
+{
+	
+	private static boolean preferredRain;
+	private String zipCode;
+	
+	private int rangeLowerBound;
+	private int rangeUpperBound;
+	private int preferredTempLowerBound;
+	private int preferrredTempUpperBound;
+
+	
+	
+	
+	
+	// boolean to describe if it is raining or not
+	// false = no rain
+	// true = rain
+	
+	public static void noRain() 
+    {
+
+        preferredRain = false;
+
+    }
+		
+	
+	public static boolean yesRain()
+    {
+
+        return preferredRain == true;
+
+    }
+ 
+	
+	public CamperPreferences() 
+
+	{
+	
+		
+
+
+	
+	
+	
+>>>>>>> 84c37b6 ++
 		// TODO Auto-generated constructor stub
 	}
 
+<<<<<<< Upstream, based on origin/Finalized_Project
 	// getters and setters
 	
 	public boolean isPreferredRain() 
@@ -99,5 +150,95 @@ public class CamperPreferences
 		System.out.println(preferredRain); 
 	
 	}
+=======
+
+	
+	
+	
+	// getters and setters
+	
+	public boolean isPreferredRain() 
+	
+	{
+		return preferredRain;
+	}
+
+	
+	public void setPreferredRain(boolean preferredRain) 
+	{
+		this.preferredRain = preferredRain;
+	}
+
+
+	public String getZipCode() 
+	{
+		return zipCode;
+	}
+
+
+	public void startZipCode(String zipCode) 
+
+	{
+		this.zipCode = zipCode;
+	}
+
+	
+	public int getRangeLowerBound() {
+		return rangeLowerBound;
+	}
+
+
+	public void setRangeLowerBound(int rangeLowerBound) {
+		this.rangeLowerBound = rangeLowerBound;
+	}
+
+
+	public int getRangeUpperBound() {
+		return rangeUpperBound;
+	}
+
+
+	public void setRangeUpperBound(int rangeUpperBound) {
+		this.rangeUpperBound = rangeUpperBound;
+	}
+
+
+	public int getPreferredTempLowerBound() {
+		return preferredTempLowerBound;
+	}
+
+
+	public void setPreferredTempLowerBound(int preferredTempLowerBound) {
+		this.preferredTempLowerBound = preferredTempLowerBound;
+	}
+
+
+	public int getPreferrredTempUpperBound() {
+		return preferrredTempUpperBound;
+	}
+
+
+	public void setPreferrredTempUpperBound(int preferrredTempUpperBound) {
+		this.preferrredTempUpperBound = preferrredTempUpperBound;
+	}
+
+	
+	
+	
+	
+	//main
+	public static void main(String[] args) 
+	
+	{
+		
+		
+		// return rain preference 
+		System.out.println(preferredRain);
+
+      
+	
+	}
+
+>>>>>>> 84c37b6 ++
 
 }
